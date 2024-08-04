2dc56f68f4c75b51bfde6ae24f094f987a1a501a
v0c03uq <vikash.chauhan@walmart.com>
2024-08-04 12:14:28 +0530

    count items, total cost

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 import { A } from '@solidjs/router';
 import { Component, createSignal } from 'solid-js';
+import { useCartContext } from '../context/CartContext';
 
 const Header: Component = () => {
     const [darkTheme, setDarkTheme] = createSignal<boolean>(false);
+    const { items } = useCartContext();
 
     const toggleTheme = () => {
         setDarkTheme(!darkTheme());
     };
+
+    const getQty = () => {
+        return items.reduce((prev, curr) => {
+            return prev + curr.qty;
+        }, 0);
+    };
+
     return (
         <header
             class="my-4 p-2 text-xl flex items-center gap-4"
@@ -24,7 +33,7 @@ const Header: Component = () => {
             <h1>Ninja Merch</h1>
 
             <A href="/">Home</A>
-            <A href="/cart">Cart</A>
+            <A href="/cart">Cart ({getQty()})</A>
         </header>
     );
 };
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,21 +1,35 @@
-import { Component, For } from 'solid-js';
+import { Component, For, Show } from 'solid-js';
+import Card from '../components/Card';
 import { useCartContext } from '../context/CartContext';
-import { CartItem } from '../interfaces';
 
 const Cart: Component = () => {
     const { items } = useCartContext();
 
+    const total = () => {
+        return items.reduce((acc, p) => {
+            return acc + p.qty * p.price;
+        }, 0);
+    };
+
     return (
-        <>
-            <div>Shopping cart</div>
-            <For each={items}>
-                {(item: CartItem) => (
-                    <p>
-                        {item.title} - ${item.price} x {item.qty}
+        <div class="max-w-md my-8 mx-auto">
+            <Card rounded={true} flat={false}>
+                <h2>Your Shopping Cart</h2>
+
+                <Show when={items.length} fallback={<p>Your cart is empty.</p>}>
+                    <For each={items}>
+                        {(item) => (
+                            <p class="my-3">
+                                {item.title} - £{item.price} x {item.qty}
+                            </p>
+                        )}
+                    </For>
+                    <p class="mt-8 pt-4 border-t-2 font-bold">
+                        Total cart price - £{total()}
                     </p>
-                )}
-            </For>
-        </>
+                </Show>
+            </Card>
+        </div>
     );
 };
 
